a4ad077585c0125e049d2f0ad130b6831080abeb
cmdr2 <secondary.cmdr2@gmail.com>
2023-02-18 14:27:47 +0530

    Upgrade transformers to 4.26.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = [
     "omegaconf==2.1.1",
     "test-tube>=0.7.5",
     "einops==0.3.0",
-    "transformers==4.19.2",
+    "transformers==4.26.1",
     "kornia==0.6",
     "open_clip_torch==2.0.2",
     "torchmetrics==0.6.0",
@@ -16,8 +16,8 @@ dependencies = [
     "huggingface-hub>=0.10.0",
 ]
 
-if platform.system() == 'Darwin':
-    dependencies += ['torch', 'torchvision']
+if platform.system() == "Darwin":
+    dependencies += ["torch", "torchvision"]
 
 # On Windows and Linux, the user needs to install torch and torchvision manually, since the CUDA versions are not hosted on PyPI.
 # They need to run: pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
